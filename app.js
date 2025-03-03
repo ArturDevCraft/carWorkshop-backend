55f44415ea2b6ee4a201ef989fedde072dd51a81
Artur <artur.czerwiak@gmail.com>
2025-03-03 14:30:13 +0100

    Create app skeleton for authentication implement

---
+++ app.js
@@ -0,0 +1,29 @@
+import fs from 'node:fs/promises';
+const express = require('express');
+import bodyParser from 'body-parser';
+
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+app.use(authRoutes);
+
+// app.get('/login', (req, res) => {});
+
+app.post();
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/signup', async (req, resizeBy, next) => {
+	const data = req.body;
+	let errors = {};
+});
+
+router.post('/login', async (req, resizeBy, next) => {
+	const data = req.body;
+	let errors = {};
+});
